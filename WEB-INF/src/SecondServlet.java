5a114a61f6dc2dd94dd1b37633820029e28448a1
indeerad <58538346+indeerad@users.noreply.github.com>
2020-05-04 20:00:15 +0530

    Second Activity
    Sample Sevlet using GenericServlet abstract class for simplicity.

---
+++ WEB-INF/src/SecondServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SecondServlet extends GenericServlet {
+
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        
+        res.getWriter().print("<h1>Welcome to Second Servlet</h1>");
+
+    }
+    
+
+}
